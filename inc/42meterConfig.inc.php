fc15d26f2f9599be17b78eb724e4d2d1be5555b2
ninthday <jeffy@ninthday.info>
2015-06-26 18:21:45 +0800

    little change

---
+++ inc/42meterConfig.inc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 42 Meters API Configration file
+ * 
+ * @author ninthday <bee.me@ninthday.info>
+ */
+
+$api_url = 'https://42matters.com/api/1/apps/lookup.json?p=';
+$access_token = '2574b3d5e48832668008a98db6ab8f1d6a123a01';
